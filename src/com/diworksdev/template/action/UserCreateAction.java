eb0f9d9e1c9406f8051b73065d0897a0859e6fe9
mk-050 <yoshikawa.0505@outlook.jp>
2024-10-05 14:16:26 +0900

    4時間目 27ページまで

---
+++ src/com/diworksdev/template/action/UserCreateAction.java
@@ -0,0 +1,11 @@
+package com.diworksdev.template.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserCreateAction extends ActionSupport {
+
+	public String excecute() {
+		return SUCCESS;
+	}
+
+}
